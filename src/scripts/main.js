aaf596a5bfd4b49dfa5bca3ed9f78301652f7d88
Simon Emms <simon@simonemms.com>
2017-07-30 17:06:35 +0100

    Got a basic vue router setup working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,25 @@
 /* Node modules */
 
 /* Third-party modules */
+import Vue from 'vue/dist/vue.min';
+import VueRouter from 'vue-router';
 
 /* Files */
+
+Vue.use(VueRouter);
+
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
