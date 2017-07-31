e180d61fcb559f0b5ced0a1328aed018146cfbc0
Simon Emms <simon@simonemms.com>
2017-07-31 20:16:57 +0100

    Added electron locale finder

--- src/scripts/i18n/index.js
+++ src/scripts/i18n/index.js
@@ -3,10 +3,12 @@
  */
 
 /* Node modules */
+import path from 'path';
 
 /* Third-party modules */
 import i18next from 'i18next';
 import i18nextFsBackend from 'i18next-sync-fs-backend';
+import i18nextLanguageDetector from 'i18next-electron-language-detector';
 import Vue from 'vue/dist/vue.min';
 import VueI18Next from '@panter/vue-i18next';
 
@@ -15,10 +17,11 @@ import VueI18Next from '@panter/vue-i18next';
 Vue.use(VueI18Next);
 
 i18next
+  .use(i18nextLanguageDetector)
   .use(i18nextFsBackend)
   .init({
     backend: {
-      loadPath: `${__dirname}/../locales/{{lng}}/{{ns}}.json`,
+      loadPath: path.join(__dirname, '..', 'locales', '{{lng}}', '{{ns}}.json'),
     },
     debug: true,
     defaultNS: 'common',
