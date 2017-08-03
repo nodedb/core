a32c731a4ceea2960ee4fe5f292f289ba13bd6e4
Simon Emms <simon@simonemms.com>
2017-08-03 07:40:29 +0100

    Added a noddy test

---
+++ test/unit/renderer/main.test.js
@@ -0,0 +1,29 @@
+/**
+ * main.test
+ */
+
+/* Node modules */
+
+/* Third-party modules */
+
+/* Files */
+import { expect, proxyquire } from '../../helpers/configure';
+import App from '../../../src/renderer/App.vue';
+
+describe('main tests', function () {
+
+  beforeEach(function () {
+    this.main = proxyquire('../../src/app/main', {});
+  });
+
+  it('should ', function () {
+
+    expect(2).to.be.equal(2);
+
+    expect(this.main).to.not.be.undefined;
+    expect(App).to.be.an('object');
+    expect(App.name).to.be.equal('my-project');
+
+  });
+
+});
