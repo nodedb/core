30cbb80af7b7d6b04a6e9bffa584a30cfe075eb0
Simon Emms <simon@simonemms.com>
2017-08-09 07:50:35 +0100

    Created a base class, which gets us the logger instance

---
+++ src/renderer/lib/base.js
@@ -0,0 +1,26 @@
+/**
+ * base
+ */
+
+/* Node modules */
+import { EventEmitter } from 'events';
+
+/* Third-party modules */
+import { remote } from 'electron';
+
+/* Files */
+
+export default class Base extends EventEmitter {
+
+  /**
+   * Logger
+   *
+   * Returns the logger instance
+   *
+   * @returns {*}
+   */
+  static get logger () {
+    return remote.app.logger;
+  }
+
+};
