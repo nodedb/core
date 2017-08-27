a810d84b5a3c37e9f2df8a8085161833ece9530a
Simon Emms <simon@simonemms.com>
2017-08-27 07:32:54 +0100

    Got the i18n getting the namespaces from files in the /locales/en folder

--- src/renderer/lib/i18n.js
+++ src/renderer/lib/i18n.js
@@ -7,6 +7,7 @@ import path from 'path';
 
 /* Third-party modules */
 import { remote } from 'electron';
+import { sync as glob } from 'glob';
 import i18next from 'i18next';
 import i18nextFsBackend from 'i18next-sync-fs-backend';
 import i18nextLanguageDetector from 'i18next-electron-language-detector';
@@ -18,26 +19,28 @@ import i18nLogger from './i18nLogger';
 
 Vue.use(VueI18Next);
 
+const rootPath = path.join(__dirname, '..', 'locales');
+
+/* Treat the en directory as "master" */
+const ns = glob(`${rootPath}/en/*.json`)
+  .map(filepath => filepath
+    .replace(`${rootPath}/en/`, '')
+    .replace(/\.json$/, ''));
+
 i18next
   .use(i18nLogger(remote.app.logger.bunyan))
   .use(i18nextLanguageDetector)
   .use(i18nextFsBackend)
   .init({
     backend: {
-      loadPath: path.join(__dirname, '..', 'locales', '{{lng}}', '{{ns}}.json'),
-      addPath: path.join(__dirname, '..', 'locales', '{{lng}}', '{{ns}}.missing.json'),
+      loadPath: path.join(rootPath, '{{lng}}', '{{ns}}.json'),
+      addPath: path.join(rootPath, '{{lng}}', '{{ns}}.missing.json'),
     },
     debug: true,
     defaultNS: 'common',
     fallbackLng: 'en',
     initImmediate: false,
-    ns: [
-      'buttons',
-      'common',
-      'connect',
-      'error',
-      'modal',
-    ],
+    ns,
   });
 
 /* Set the i18next instance to the app */
