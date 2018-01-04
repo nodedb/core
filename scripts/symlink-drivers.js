0b4669452123a135f72e3a7337aece2b905589f6
Simon Emms <simon@simonemms.com>
2018-01-04 21:06:00 +0000

    Avoid symlinking becoming an error

--- scripts/symlink-drivers.js
+++ scripts/symlink-drivers.js
@@ -17,24 +17,38 @@ const driverPath = path.join(projectPath, '..');
 
 const drivers = fs.readdirSync(driverPath);
 
-fs.mkdirpSync(target);
+try {
 
-drivers.forEach((driver) => {
-  const fullTargetPath = path.join(target, driver);
-  const fullDriverPath = path.join(driverPath, driver);
+  fs.mkdirpSync(target);
 
-  const stat = fs.statSync(fullDriverPath);
+  const driversLinked = [];
 
-  if (stat.isDirectory() === false) {
-    /* Not a directory */
-    return;
-  }
+  drivers.forEach((driver) => {
+    const fullTargetPath = path.join(target, driver);
+    const fullDriverPath = path.join(driverPath, driver);
 
-  if (fullDriverPath === projectPath) {
-    /* It's this project */
-    return;
-  }
+    const stat = fs.statSync(fullDriverPath);
 
-  /* It's a driver - symlink it */
-  fs.ensureSymlinkSync(fullDriverPath, fullTargetPath);
-});
+    if (stat.isDirectory() === false) {
+      /* Not a directory */
+      return;
+    }
+
+    if (fullDriverPath === projectPath) {
+      /* It's this project */
+      return;
+    }
+
+    /* It's a driver - symlink it */
+    fs.ensureSymlinkSync(fullDriverPath, fullTargetPath);
+
+    driversLinked.push(driver);
+  });
+
+  console.log(`Linked ${driversLinked.length} driver(s)`);
+  console.log(driversLinked);
+} catch (err) {
+  console.error('Failed to symlink drivers');
+  console.error(err.stack);
+  process.exit(1);
+}
